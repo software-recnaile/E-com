ee05194b852308c7cb591635a4632b979be402eb
software-recnaile <software@recnaile.com>
2025-07-21 17:36:57 +0530

    Admin credential service

---
+++ oauthService/src/main/java/com/recnaile/oauthService/oauthServiceApplication.java
@@ -0,0 +1,16 @@
+package com.recnaile.oauthService;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class oauthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(oauthServiceApplication.class, args);
+    }
+
+}
+
