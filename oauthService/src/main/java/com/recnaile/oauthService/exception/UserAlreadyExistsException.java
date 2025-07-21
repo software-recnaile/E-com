55512cfb698d8e701d5bf5a8eb8db910d0980b19
software-recnaile <software@recnaile.com>
2025-07-21 17:35:15 +0530

    Delete oauthService directory

--- oauthService/src/main/java/com/recnaile/oauthService/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.recnaile.oauthService.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- oauthService/src/main/java/com/recnaile/oauthService/exception/UserNotVerifiedException.java
+++
@@ -1,7 +0,0 @@
-package com.recnaile.oauthService.exception;
-
-public class UserNotVerifiedException extends RuntimeException {
-    public UserNotVerifiedException(String message) {
-        super(message);
-    }
-}
