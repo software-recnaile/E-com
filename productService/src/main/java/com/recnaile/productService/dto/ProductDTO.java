4f876b4931629af4362f45fbfbf61b6c13dea0f3
software-recnaile <software@recnaile.com>
2025-07-22 14:43:03 +0530

    Product Service

---
+++ productService/src/main/java/com/recnaile/productService/dto/ProductDTO.java
@@ -0,0 +1,28 @@
+package com.recnaile.productService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private String productName;
+    private List<String> productUsecase;
+    private List<String> productImages;
+    private String productThumbnail;
+    private String productDescription;
+    private String productCategory;
+    private String productSubCategory;
+    private Map<String, String> productSpecialization;
+    private List<Map<String, String>> productVariants;
+    private Integer availableStock;
+    private Double mrpRate;
+    private Double discountAmount;
+}
