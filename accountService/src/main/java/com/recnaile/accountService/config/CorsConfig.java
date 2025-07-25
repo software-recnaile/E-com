0438f839df8ceff16efe4d6a9c29c042faba551d
software-recnaile <software@recnaile.com>
2025-07-25 16:13:49 +0530

    Update CorsConfig.java

--- accountService/src/main/java/com/recnaile/accountService/config/CorsConfig.java
+++ accountService/src/main/java/com/recnaile/accountService/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.recnaile.productService.config;
+package com.recnaile.accountService.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
