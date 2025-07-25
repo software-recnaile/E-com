f26a26b4b472c81999731ae33ef6830308591d29
software-recnaile <software@recnaile.com>
2025-07-25 12:35:43 +0530

    Update AuthResponse.java

--- authService/src/main/java/com/recnaile/authService/dto/AuthResponse.java
+++ authService/src/main/java/com/recnaile/authService/dto/AuthResponse.java
@@ -1,10 +1,15 @@
 package com.recnaile.authService.dto;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
 
 @Data
 public class AuthResponse {
     private String token;
     private String email;
     private String username;
+    private String id;
+    public void setId(ObjectId id) {
+        this.id = id.toString(); // or just store as ObjectId
+    }
 }
