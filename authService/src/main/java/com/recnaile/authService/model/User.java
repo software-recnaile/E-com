6f786a896eac590328857656301fe09c87e92056
software-recnaile <software@recnaile.com>
2025-07-25 12:36:47 +0530

    Update User.java

--- authService/src/main/java/com/recnaile/authService/model/User.java
+++ authService/src/main/java/com/recnaile/authService/model/User.java
@@ -1,68 +1,7 @@
-//package com.recnaile.authService.model;
-//
-//import lombok.Data;
-//import org.springframework.data.annotation.Id;
-//import org.springframework.data.mongodb.core.index.Indexed;
-//import org.springframework.data.mongodb.core.mapping.Document;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Collection;
-//import java.util.Collections;
-//import java.util.Date;
-//
-//@Data
-//@Document(collection = "users")
-//public class User implements UserDetails {
-//    @Id
-//    private String id;
-//
-//    private String username;
-//
-//    @Indexed(unique = true)
-//    private String email;
-//
-//    private String password;
-//
-//    private boolean enabled = false;
-//    private boolean accountNonExpired = true;
-//    private boolean accountNonLocked = true;
-//    private boolean credentialsNonExpired = true;
-//
-//    private Date createdDate;
-//    private Date lastModifiedDate;
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return accountNonExpired;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return accountNonLocked;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return credentialsNonExpired;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return enabled;
-//    }
-//}
-
-
 package com.recnaile.authService.model;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -78,7 +17,7 @@ import java.util.Date;
 @Document(collection = "users")
 public class User implements UserDetails {
     @Id
-    private String id;
+    private ObjectId id;
 
     private String username;
 
