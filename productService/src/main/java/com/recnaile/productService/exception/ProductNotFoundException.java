fd7725296635627b52c779805da97523c61dd707
software-recnaile <software@recnaile.com>
2025-07-28 13:04:26 +0530

    Delete productService directory

--- productService/src/main/java/com/recnaile/productService/exception/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.recnaile.productService.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-  public ProductNotFoundException(String message) {
-    super(message);
-  }
-}
