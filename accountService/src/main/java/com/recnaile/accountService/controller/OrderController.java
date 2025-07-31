974187c621702803130568e0d0470c5030204ac6
software-recnaile <software@recnaile.com>
2025-07-31 11:51:59 +0530

    Update OrderController.java

--- accountService/src/main/java/com/recnaile/accountService/controller/OrderController.java
+++ accountService/src/main/java/com/recnaile/accountService/controller/OrderController.java
@@ -1,45 +1,4 @@
-//package com.recnaile.accountService.controller;
-//
-//import com.recnaile.accountService.model.Order;
-//import com.recnaile.accountService.service.OrderService;
-//import jakarta.validation.constraints.NotBlank;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/orders")
-//public class OrderController {
-//    private final OrderService orderService;
-//
-//    public OrderController(OrderService orderService) {
-//        this.orderService = orderService;
-//    }
-//
-//    @PostMapping("/place-order/user/{userId}")
-//    public ResponseEntity<Order> placeOrder(
-//            @PathVariable String userId,
-//            @RequestParam @NotBlank String shippingAddressId,
-//            @RequestParam @NotBlank String billingAddressId,
-//            @RequestParam @NotBlank String paymentMethod) {
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(orderService.placeOrder(userId, shippingAddressId, billingAddressId, paymentMethod));
-//    }
-//
-//    @GetMapping("/user/{userId}")
-//    public ResponseEntity<List<Order>> getUserOrders(@PathVariable String userId) {
-//        return ResponseEntity.ok(orderService.getUserOrders(userId));
-//    }
-//
-//    @GetMapping("/{orderNumber}/user/{userId}")
-//    public ResponseEntity<Order> getOrder(
-//            @PathVariable String orderNumber,
-//            @PathVariable String userId) {
-//        return ResponseEntity.ok(orderService.getOrder(orderNumber, userId));
-//    }
-//}
+
 
 package com.recnaile.accountService.controller;
 
@@ -83,6 +42,10 @@ public class OrderController {
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Order>> getUserOrders(@PathVariable String userId) {
         return ResponseEntity.ok(orderService.getUserOrders(userId));
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return ResponseEntity.ok(orderService.getAllOrders());
     }
 
     @GetMapping("/{orderNumber}/user/{userId}")
