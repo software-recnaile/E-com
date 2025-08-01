9d275d8f817016957579fcadfa11a6db4e6db43c
software-recnaile <software@recnaile.com>
2025-08-01 15:25:00 +0530

    Bulk Order Service

---
+++ bulkorderservice/src/test/java/com/recnaile/bulkorderservice/BulkorderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recnaile.bulkorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkorderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
