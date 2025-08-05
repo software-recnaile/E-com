93fdcaa3cce8c534903e492c3f72d9da5cf8b7f6
software-recnaile <software@recnaile.com>
2025-08-05 18:06:17 +0530

    Wishlist service

---
+++ wishlistService/src/test/java/com/recnaile/wishlistService/WishlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recnaile.wishlistService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
