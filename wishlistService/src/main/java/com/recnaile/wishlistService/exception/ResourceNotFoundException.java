1fd11045c35f34f2653374d848b0070ec6da03c7
software-recnaile <software@recnaile.com>
2025-08-05 18:04:21 +0530

    Delete wishlistService directory

--- wishlistService/src/main/java/com/recnaile/wishlistService/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.recnaile.wishlistService.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
