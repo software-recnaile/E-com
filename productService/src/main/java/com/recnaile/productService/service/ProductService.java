a16567a76ed9f753275e20db6e5d1bdf668af5bb
software-recnaile <software@recnaile.com>
2025-08-05 09:49:20 +0530

    Update ProductService.java

--- productService/src/main/java/com/recnaile/productService/service/ProductService.java
+++ productService/src/main/java/com/recnaile/productService/service/ProductService.java
@@ -1,30 +1,3 @@
-//package com.recnaile.productService.service;
-//
-//import com.recnaile.productService.dto.ProductDTO;
-//import com.recnaile.productService.dto.ProductResponse;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//public interface ProductService {
-//    ProductResponse addProduct(ProductDTO productDTO, MultipartFile[] images) throws IOException;
-//    ProductResponse getProductById(String id);
-//    ProductResponse getProductByUniqueName(String uniqueName);
-//    List<ProductResponse> getAllProducts();
-//    List<ProductResponse> getProductsByCategory(String category);
-//    List<ProductResponse> getProductsByCategoryAndSubCategory(String category, String subCategory);
-//    List<ProductResponse> searchProducts(String query);
-//    ProductResponse updateProduct(String id, ProductDTO productDTO);
-//    void deleteProduct(String id);
-//    ProductResponse updateStock(String id, Integer quantity);
-//
-//    // Image management methods
-//    ProductResponse uploadProductImages(String id, MultipartFile[] images) throws IOException;
-//    ProductResponse updateProductImages(String id, MultipartFile[] images) throws IOException;
-//    ProductResponse updateSingleImage(String id, int imageIndex, MultipartFile image) throws IOException;
-//    void deleteProductImages(String id) throws IOException;
-//}
 
 package com.recnaile.productService.service;
 
@@ -50,5 +23,6 @@ public interface ProductService {
     ProductResponse updateSingleImage(String id, int imageIndex, MultipartFile image) throws IOException;
     void deleteProductImages(String id) throws IOException;
     void deleteSingleImage(String id, int imageIndex) throws IOException;
+    ProductResponse updateStockByUniqueName(String uniqueName, Integer quantity);
 
 }
