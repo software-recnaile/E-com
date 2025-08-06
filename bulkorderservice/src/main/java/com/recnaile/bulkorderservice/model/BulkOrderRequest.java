be15a04ef9f692c89c201068a11da225536beef1
software-recnaile <software@recnaile.com>
2025-08-06 09:54:01 +0530

    Bulk Order Service

---
+++ bulkorderservice/src/main/java/com/recnaile/bulkorderservice/model/BulkOrderRequest.java
@@ -0,0 +1,27 @@
+package com.recnaile.bulkorderservice.model;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Data
+public class BulkOrderRequest {
+    private String companyName;
+    private String contactPerson;
+    private String email;
+    private String phone;
+    private String quantity;
+    private String deliveryDate;
+    private String additionalNotes;
+    private String companyType;
+    private String taxId;
+    private String shippingAddress;
+    private String billingAddress;
+    private String paymentTerms;
+    private boolean sameAsShipping;
+    private Map<String, List<String>> selectedProducts;
+
+
+}
