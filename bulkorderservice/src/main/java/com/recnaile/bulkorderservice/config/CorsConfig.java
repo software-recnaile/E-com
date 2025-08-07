f76c79f79787dd801cd7e1adb7780c213ba35c2e
software-recnaile <software@recnaile.com>
2025-08-07 12:52:19 +0530

    Update and rename WebConfig.java to CorsConfig.java

--- bulkorderservice/src/main/java/com/recnaile/bulkorderservice/config/CorsConfig.java
+++ bulkorderservice/src/main/java/com/recnaile/bulkorderservice/config/CorsConfig.java
@@ -1,19 +1,17 @@
 package com.recnaile.bulkorderservice.config;
 
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedOrigins("*") // Your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
-
