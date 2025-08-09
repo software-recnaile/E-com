10fdbf7bd55d55c608d2007890643c9d4ab230c5
software-recnaile <software@recnaile.com>
2025-08-09 09:49:53 +0530

    Auth Service

---
+++ authService/src/main/java/com/recnaile/authService/dto/OtpRequest.java
@@ -0,0 +1,15 @@
+package com.recnaile.authService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class OtpRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String otp;
+}
