a9a95ed98eca346bab9de82917666eff8c90d543
software-recnaile <software@recnaile.com>
2025-08-09 09:23:38 +0530

    Auth Service

---
+++ authService/src/main/java/com/recnaile/authService/authServiceApplication.java
@@ -0,0 +1,16 @@
+package com.recnaile.authService;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class authServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(authServiceApplication.class, args);
+    }
+
+}
+
