5d16b0b0240726b6342f996f3e7b2e5aeda58cc3
software-recnaile <software@recnaile.com>
2025-08-09 09:23:38 +0530

    Auth Service

---
+++ authService/src/main/java/com/recnaile/authService/model/User.java
@@ -0,0 +1,132 @@
+//package com.recnaile.authService.model;
+//
+//import lombok.Data;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.mongodb.core.index.Indexed;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//import java.util.Collections;
+//import java.util.Date;
+//
+//@Data
+//@Document(collection = "users")
+//public class User implements UserDetails {
+//    @Id
+//    private String id;
+//
+//    private String username;
+//
+//    @Indexed(unique = true)
+//    private String email;
+//
+//    private String password;
+//
+//    private boolean enabled = false;
+//    private boolean accountNonExpired = true;
+//    private boolean accountNonLocked = true;
+//    private boolean credentialsNonExpired = true;
+//
+//    private Date createdDate;
+//    private Date lastModifiedDate;
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return accountNonExpired;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return accountNonLocked;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return credentialsNonExpired;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return enabled;
+//    }
+//}
+
+
+package com.recnaile.authService.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+
+@Data
+@Document(collection = "users")
+public class User implements UserDetails {
+    @Id
+    private String id;
+
+    private String username;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    private boolean enabled = false;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+
+    private Date createdDate;
+    private Date lastModifiedDate;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
