ac4469fb530205ba01394604ecb3231eccc882a0
software-recnaile <software@recnaile.com>
2025-08-09 09:49:53 +0530

    Auth Service

---
+++ authService/src/main/java/com/recnaile/authService/dto/PasswordResetRequest.java
@@ -0,0 +1,25 @@
+//package com.recnaile.authService.dto;
+//
+//import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.NotBlank;
+//import lombok.Data;
+//
+//@Data
+//public class PasswordResetRequest {
+//    @NotBlank
+//    @Email
+//    private String email;
+//}
+
+package com.recnaile.authService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PasswordResetRequest {
+    @NotBlank
+    @Email
+    private String email;
+}
