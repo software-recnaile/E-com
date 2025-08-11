dbcd46402ddb65206728c4226393cc1e6d67d022
software-recnaile <software@recnaile.com>
2025-08-11 16:37:08 +0530

    Delete mailService directory

--- mailService/src/main/java/com/recnaile/mailService/repository/ActivityLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recnaile.mailService.repository;
-
-import com.recnaile.mailService.model.ActivityLogDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityLogRepository extends MongoRepository<ActivityLogDocument, String> {
-}
