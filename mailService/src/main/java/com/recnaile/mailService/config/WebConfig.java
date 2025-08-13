70237b1cc22f434fcfac6083247496be16bb0acf
software-recnaile <software@recnaile.com>
2025-08-13 16:35:44 +0530

    Update WebConfig.java

--- mailService/src/main/java/com/recnaile/mailService/config/WebConfig.java
+++ mailService/src/main/java/com/recnaile/mailService/config/WebConfig.java
@@ -9,10 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")  // Changed to cover all endpoints
-                .allowedOrigins("http://localhost:5173")
+        registry.addMapping("/api/**, /**")  // Changed to cover all endpoints
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")  // Added PATCH
                 .allowedHeaders("*")
                 .allowCredentials(true);  // Added if you need credentials
     }
 }
+
