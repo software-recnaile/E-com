ffaf723730501971f877712870bedadca44ef20b
software-recnaile <software@recnaile.com>
2025-08-14 17:21:54 +0530

    Order service

---
+++ accountService/src/main/java/com/recnaile/accountService/config/AccountDbProperties.java
@@ -0,0 +1,12 @@
+package com.recnaile.accountService.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.data.mongodb.account-db")
+public class AccountDbProperties {
+    private String uri;
+}
