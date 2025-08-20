7151fd94c3b213c83dd5f9346ab7e6d88f891ff3
software-recnaile <software@recnaile.com>
2025-08-20 10:07:15 +0530

    Delete accountService directory

--- accountService/src/main/java/com/recnaile/accountService/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.recnaile.accountService.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
