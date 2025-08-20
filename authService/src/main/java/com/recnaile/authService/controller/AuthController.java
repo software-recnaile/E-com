f6a21d6e22cef41ae80f1e5d4caf350ff04dbe00
software-recnaile <software@recnaile.com>
2025-08-20 12:19:29 +0530

    Update AuthController.java

--- authService/src/main/java/com/recnaile/authService/controller/AuthController.java
+++ authService/src/main/java/com/recnaile/authService/controller/AuthController.java
@@ -1,74 +1,3 @@
-//
-//
-//package com.recnaile.authService.controller;
-//
-//import com.recnaile.authService.dto.*;
-//import com.recnaile.authService.service.AuthService;
-//import com.recnaile.authService.service.OtpService;
-//import com.recnaile.authService.service.UserService;
-//import jakarta.validation.Valid;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//public class AuthController {
-//    @Autowired
-//    private AuthService authService;
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @Autowired
-//    private OtpService otpService;
-//
-//    @PostMapping("/signup")
-//    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
-//        return ResponseEntity.ok(userService.registerUser(signUpRequest));
-//    }
-//
-//    @PostMapping("/verify")
-//    public ResponseEntity<?> verifyUser(@Valid @RequestBody OtpRequest otpRequest) {
-//        if (otpService.verifyOtp(otpRequest.getEmail(), otpRequest.getOtp())) {
-//            userService.verifyUser(otpRequest.getEmail());
-//            return ResponseEntity.ok("User verified successfully");
-//        }
-//        return ResponseEntity.badRequest().body("Invalid OTP or OTP expired");
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<?> authenticateUser(@Valid @RequestBody AuthRequest authRequest) {
-//        try {
-//            AuthResponse response = authService.authenticate(authRequest);
-//            return ResponseEntity.ok(response);
-//        } catch (Exception e) {
-//            return ResponseEntity.badRequest().body(e.getMessage());
-//        }
-//    }
-//
-//    @PostMapping("/forgot-password")
-//    public ResponseEntity<?> forgotPassword(@Valid @RequestBody PasswordResetRequest request) {
-//        userService.initiatePasswordReset(request.getEmail());
-//        return ResponseEntity.ok("Password reset OTP sent to your email");
-//    }
-//
-//    @PostMapping("/reset-password")
-//    public ResponseEntity<?> resetPassword(@Valid @RequestBody NewPasswordRequest request) {
-//        if (!request.getNewPassword().equals(request.getConfirmPassword())) {
-//            return ResponseEntity.badRequest().body("Passwords do not match");
-//        }
-//
-//        if (otpService.verifyOtp(request.getEmail(), request.getToken())) {
-//            userService.resetPassword(request.getEmail(), request.getNewPassword());
-//            return ResponseEntity.ok("Password reset successfully");
-//        }
-//        return ResponseEntity.badRequest().body("Invalid OTP or OTP expired");
-//    }
-//}
 
 
 package com.recnaile.authService.controller;
@@ -160,3 +89,4 @@ public class AuthController {
     }
 
 }
+
