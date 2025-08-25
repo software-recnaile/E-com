6a9039938b5974158894b2077a540989914021ca
software-recnaile <software@recnaile.com>
2025-08-25 09:52:27 +0530

    commended line removed

--- oauthService/src/main/java/com/recnaile/oauthService/dto/PasswordResetRequest.java
+++ oauthService/src/main/java/com/recnaile/oauthService/dto/PasswordResetRequest.java
@@ -1,16 +1,3 @@
-//package com.recnaile.authService.dto;
-//
-//import jakarta.validation.constraints.Email;
-//import jakarta.validation.constraints.NotBlank;
-//import lombok.Data;
-//
-//@Data
-//public class PasswordResetRequest {
-//    @NotBlank
-//    @Email
-//    private String email;
-//}
-
 package com.recnaile.oauthService.dto;
 
 import jakarta.validation.constraints.Email;
