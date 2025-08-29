253e38d39d1f4e7e416abbcb0f3e888d1c769222
software-recnaile <software@recnaile.com>
2025-08-29 14:16:50 +0530

    Update WebConfig.java

--- mailService/src/main/java/com/recnaile/mailService/config/WebConfig.java
+++ mailService/src/main/java/com/recnaile/mailService/config/WebConfig.java
@@ -10,13 +10,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
                         registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                                      "http://localhost:8081")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
                        
     }
 }
+
 
 
 
