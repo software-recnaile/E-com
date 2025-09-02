ee8c18883a07fda0db1ad850a72f252b8fc5f90a
software-recnaile <software@recnaile.com>
2025-09-02 11:54:32 +0530

    Delete authService directory

--- authService/src/main/java/com/recnaile/authService/repository/UserActivityLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.recnaile.authService.repository;
-
-import com.recnaile.authService.model.UserActivityLog;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserActivityLogRepository extends MongoRepository<UserActivityLog, String> {
-}
