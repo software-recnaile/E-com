5a5bc3449ad7ff186802c92de72d4a6e2420caea
software-recnaile <software@recnaile.com>
2025-11-18 04:26:58 +0530

    Update CorsConfig.java

--- productService/src/main/java/com/recnaile/productService/config/CorsConfig.java
+++ productService/src/main/java/com/recnaile/productService/config/CorsConfig.java
@@ -16,8 +16,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Apply to all endpoints
-                .allowedOrigins("http://localhost:5173",
-                               "http://localhost:8081")  // Allow all origins
+                .allowedOrigins("*",
+                               "*")  // Allow all origins
                 .allowedMethods("*")  // Allow all HTTP methods
                 .allowedHeaders("*"); // Allow all headers
     }
@@ -37,3 +37,4 @@ public class CorsConfig implements WebMvcConfigurer {
     }
 
 }
+
